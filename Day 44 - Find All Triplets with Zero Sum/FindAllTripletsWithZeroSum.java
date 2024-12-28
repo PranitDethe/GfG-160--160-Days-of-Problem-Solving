17bed90dffc06fb4b514b998ea4ebbdf97dcef50
PranitDethe <103254744+PranitDethe@users.noreply.github.com>
2024-12-28 16:02:20 +0530

    Create FindAllTripletsWithZeroSum.java

---
+++ Day 44 - Find All Triplets with Zero Sum/FindAllTripletsWithZeroSum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> findTriplets(int[] arr) {
+        // Your code here
+        List<List<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        
+                        List<Integer> triplet = Arrays.asList(i, j, k);
+                        
+                        Collections.sort(triplet);
+                        
+                        if(!result.contains(triplet)) {
+                            result.add(triplet);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
