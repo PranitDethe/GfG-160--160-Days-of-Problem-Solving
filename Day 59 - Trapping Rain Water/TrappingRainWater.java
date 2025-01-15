eb7410a99b03b8244e5e383efc39c2f26d16e552
PranitDethe <103254744+PranitDethe@users.noreply.github.com>
2025-01-15 16:09:49 +0530

    Create TrappingRainWater.java

---
+++ Day 59 - Trapping Rain Water/TrappingRainWater.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        int n = arr.length;
+
+        // Two pointers
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+
+        while (left <= right) {
+            if (arr[left] <= arr[right]) {
+                // Update leftMax and calculate water trapped
+                if (arr[left] >= leftMax) {
+                    leftMax = arr[left];
+                } else {
+                    totalWater += leftMax - arr[left];
+                }
+                left++;
+            } else {
+                // Update rightMax and calculate water trapped
+                if (arr[right] >= rightMax) {
+                    rightMax = arr[right];
+                } else {
+                    totalWater += rightMax - arr[right];
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+}
