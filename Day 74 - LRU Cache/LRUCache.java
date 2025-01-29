5c981afc8d1638d4ef662a53fa781b24281c6afa
PranitDethe <103254744+PranitDethe@users.noreply.github.com>
2025-01-29 16:18:44 +0530

    Create LRUCache.java

---
+++ Day 74 - LRU Cache/LRUCache.java
@@ -0,0 +1,71 @@
+class LRUCache {
+    private static class Node {
+        int key, value;
+        Node prev, next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> cache;
+    private final Node head, tail;
+
+    public LRUCache(int cap) {
+        this.capacity = cap;
+        this.cache = new HashMap<>();
+        this.head = new Node(0, 0); 
+        this.tail = new Node(0, 0); 
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            moveToHead(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            if (cache.size() > capacity) {
+                Node tailNode = removeTail();
+                cache.remove(tailNode.key);
+            }
+        }
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private void addToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private Node removeTail() {
+        Node tailNode = tail.prev;
+        removeNode(tailNode);
+        return tailNode;
+    }
+}
