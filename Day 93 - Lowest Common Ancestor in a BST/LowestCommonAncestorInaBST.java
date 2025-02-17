cafbe881db86d0612c875cb4a6fd068b0429a35f
PranitDethe <103254744+PranitDethe@users.noreply.github.com>
2025-02-17 16:44:11 +0530

    Create LowestCommonAncestorInaBST.java

---
+++ Day 93 - Lowest Common Ancestor in a BST/LowestCommonAncestorInaBST.java
@@ -0,0 +1,14 @@
+class Solution {
+    Node LCA(Node root, Node n1, Node n2) {
+        // your code here.
+        if (root == null) return null;
+        if(n1.data < root.data && n2.data < root.data) {
+            return LCA(root.left, n1, n2);
+        }
+        
+        if(n1.data > root.data && n2.data > root.data) {
+            return LCA(root.right, n1, n2);
+        }
+        return root;
+    }
+}
