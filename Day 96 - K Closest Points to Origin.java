790e5b560269af1e05d98ecb40d802dd456f5e6c
PranitDethe <103254744+PranitDethe@users.noreply.github.com>
2025-02-19 16:24:07 +0530

    Create Day 96 - K Closest Points to Origin.java

---
+++ Day 96 - K Closest Points to Origin.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        // Your code here
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a,b) -> Integer.compare(distance(b),distance(a))
+        );
+        
+        for(int[] point : points) {
+            maxHeap.offer(point);
+            if(maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        int[][] result = new int[k][2];
+        for(int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll ();
+        }
+        return result;
+    }
+    private int distance(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+}
